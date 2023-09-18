76d426bef845c60d04c8a8cbdfd6069aa424db31
Matheus Pires <55162066+matheuslpiresm@users.noreply.github.com>
2023-09-18 18:38:12 -0300

    Adicionando exercício de entrada e saída de dados

---
+++ python/dados-usuario.py
@@ -0,0 +1,19 @@
+#Elabore um algoritmo para que o usuário, através da entrada de dados, informe os seus dados pessoais.
+#Alguns desses dados fornecidos pelo usuário precisam ser apresentados na tela quando o algoritmo for executado, são eles:
+
+# Nome, Endereço, Cidade, CPF, RG.
+
+# Solicita os dados do usuário
+nome = input("Digite seu nome: ")
+endereco = input("Digite seu endereço: ")
+cidade = input("Digite o nome da sua cidade: ")
+cpf = input("Digite o número do seu CPF: ")
+rg = input("Digite o número do seu RG: ")
+
+# Exibe os dados informados
+print("\nOs dados informados foram:\n")
+print("Nome:", nome)
+print("Endereço:", endereco)
+print("Cidade:", cidade)
+print("CPF:", cpf)
+print("RG:", rg)
